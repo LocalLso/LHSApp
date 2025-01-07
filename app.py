887a379cebede91b7eacd6a07e799c5229f41b5f
Maphale Thulo <maphalethulocupadadeepmakhele5@gmail.com>
2025-01-07 19:29:55 +0200

    Updated the app file.

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'users.login'
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+from users.routes import users_bp
+from assessments.routes import assessments_bp
+from analytics.routes import analytics_bp
 
-if __name__ == "__main__":
+app.register_blueprint(users_bp)
+app.register_blueprint(assessments_bp)
+app.register_blueprint(analytics_bp)
+
+if __name__ == '__main__':
     app.run(debug=True)
+
